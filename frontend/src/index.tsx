6526c2288187db92c8485699046e54e885a4cbc1
pawelbareja <bareja.pawel@gmail.com>
2022-06-28 10:51:24 +0200

    add MUI ThemeProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './store/store'
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/styles';
+import { theme } from './style/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
